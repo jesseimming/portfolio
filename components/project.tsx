83be9c9d93d4fa14440f4785ddec495362fc7a09
Jesse Imming <144343797+jesseimming@users.noreply.github.com>
2024-11-06 14:48:02 +0100

    Update project.tsx

--- components/project.tsx
+++ components/project.tsx
@@ -19,6 +19,7 @@ export default function Project() {
     target: ref,
     offset: ["0 1", "1.33 1"],
   });
+  
   const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
   const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
